c09df0358b3559cf0d4b97ba0efe1a24d1c4b674
JonWang <jiongwang1984@gmail.com>
2020-01-09 10:20:09 +0100

    Create sparDens0.1.py

---
+++ sparDens0.1.py
@@ -0,0 +1,61 @@
+#-------------------------------------------------------------------------------
+# Name:        Learning spatial processes through sparse measurements 
+#              and its dense gridded covariate(s)
+
+# Purpose:     
+#              1. Aiming at the general problem of populating sparse in-situs with EO data
+#              2. Starting from artificially created dummy data
+#              3. Deducting the problems to special study cases
+#              Main method is ...
+#
+# Modifications ~~:
+#              1.
+#
+# Author:      Jiong Wang
+#
+# Created:     08/01/2020
+# Copyright:   (c) JonWang 2020
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+
+#########################
+# Create dummy sparse and dense datasets
+#########################
+
+# Gridded dummy dataset options with specified rows (r) and columns (c)
+def grid(r, c):
+
+
+
+
+
+
+
+
+#########################
+# REAL sparse and dense datasets read-in
+#########################
+
+
+
+
+
+
+
+#########################
+# Georeferencing REAL datasets
+#########################
+
+
+
+
+
+
+#########################
+# REAL Training, testing and validation
+#########################
+
+
+
+
