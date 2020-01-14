62f24881563546ff3f2e2764ee2a54e3f4ff9245
JonWang <jiongwang1984@gmail.com>
2020-01-14 17:23:18 +0100

    1-D updates
    Added toy 1D section

--- sparDens0.1.py
+++ sparDens0.1.py
@@ -19,12 +19,82 @@
 #-------------------------------------------------------------------------------
 
 
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab as pb
+import GPy
+
+#########################
+# Toy in 1-dimensional
+#########################
+
+# Experimental taste of the Gaussian Process (GP) regression
+# Sample data from a sin/cos conjunctions
+X = np.linspace(0.05,0.95,10)[:,None]
+Y = -np.cos(np.pi*X) +np.sin(4*np.pi*X) + np.random.randn(10,1)*0.05
+pb.figure()
+pb.plot(X,Y,'kx',mew=1.5)
+
+# Specify the kernel and model
+k = GPy.kern.RBF(input_dim=1, variance=1., lengthscale=1.)
+m = GPy.models.GPRegression(X,Y,k)
+m.plot()
+
+# Manually optimize the model and show trained parameters
+m['.*var']=2.
+m['.*leng']=0.5
+m['Gaussian_noise.variance']=0.001
+m.plot()
+print(m)
+
+# Automatically optimize the model and show trained parameters
+m.optimize()
+m.plot()
+print(m)
+
+# Posterior realizations of the optimized model
+testX = np.linspace(0, 1, 101).reshape(-1, 1)
+posteriorTestY = m.posterior_samples_f(testX, full_cov=True, size=3).reshape(-1,3) # Draw 3 realizations
+simY, simMse = m.predict(testX)
+
+plt.plot(testX, posteriorTestY, linewidth=.5)
+plt.plot(X, Y, 'ok', markersize=4)
+plt.plot(testX, simY - 3 * simMse ** 0.5, '--g', linewidth=.5)
+plt.plot(testX, simY + 3 * simMse ** 0.5, '--g', linewidth=.5)
+
+
 #########################
 # Create dummy sparse and dense datasets
 #########################
 
-# Gridded dummy dataset options with specified rows (r) and columns (c)
+# Gridded dummy dataset with specified function, rows (r) and columns (c)
+# The gridded dataset is realized as a random draw from 2D Gaussian Process
+
+def branin(X):
+    y = (X[:,1]-5.1/(4*np.pi**2)*X[:,0]**2+5*X[:,0]/np.pi-6)**2
+    y += 10*(1-1/(8*np.pi))*np.cos(X[:,0])+10
+    return(y)
+
+xg1 = np.linspace(-5,10,5)
+xg2 = np.linspace(0,15,5)
+X = np.zeros((xg1.size * xg2.size,2))
+for i,x1 in enumerate(xg1):
+    for j,x2 in enumerate(xg2):
+        X[i+xg1.size*j,:] = [x1,x2]
+
+Y = branin(X)[:,None]
+
+
+
+
+
+
+
+
+
+
 def grid(r, c):
+
 
 
 
